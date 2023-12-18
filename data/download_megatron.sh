7bf5f3ee66ae3f93869512a58c6d6b674fee7ff2
Yang <appledude1994@gmail.com>
2023-12-18 14:18:52 +0800

    Create download_megatron.sh

---
+++ data/download_megatron.sh
@@ -0,0 +1,4 @@
+wget "https://lsh-oss-gpt-spam.oss-cn-shanghai.aliyuncs.com/zhuran/gpu_ckpt_iter_0000100.tar.gz?OSSAccessKeyId=LTAI5tDuCoTTh6gu5PK8gFfN&Expires=1702868349&Signature=8H4eIznvhC2BYrfTXSw0doHF6rU%3D" -O gpu_ckpt_iter_0000100.tar.gz
+
+wget "https://lsh-oss-gpt-spam.oss-cn-shanghai.aliyuncs.com/zhuran/data_cache.tar.gz?OSSAccessKeyId=LTAI5tDuCoTTh6gu5PK8gFfN&Expires=1702875328&Signature=d152gh6%2FkamDqItTMX%2FdmYY89aE%3D" -O data_cache.tar.gz
+
