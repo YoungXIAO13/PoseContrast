6e4d8fa49f3c585305f7af268e3efd4a9e4c7b67
Yang <appledude1994@gmail.com>
2023-12-18 14:46:52 +0800

    Update download_megatron.sh

--- data/download_megatron.sh
+++ data/download_megatron.sh
@@ -1,4 +1,4 @@
-wget "https://lsh-oss-gpt-spam.oss-cn-shanghai.aliyuncs.com/zhuran/gpu_ckpt_iter_0000100.tar.gz?OSSAccessKeyId=LTAI5tDuCoTTh6gu5PK8gFfN&Expires=1702868349&Signature=8H4eIznvhC2BYrfTXSw0doHF6rU%3D" -O gpu_ckpt_iter_0000100.tar.gz
+wget "https://lsh-oss-gpt-spam.oss-cn-shanghai.aliyuncs.com/zhuran/gpu_ckpt_iter_0000100.tar.gz?OSSAccessKeyId=LTAI5tDuCoTTh6gu5PK8gFfN&Expires=1702885472&Signature=v80jhOipLcOsAYkKjfmR9ySGcXE%3D" -O gpu_ckpt_iter_0000100.tar.gz
 
-wget "https://lsh-oss-gpt-spam.oss-cn-shanghai.aliyuncs.com/zhuran/data_cache.tar.gz?OSSAccessKeyId=LTAI5tDuCoTTh6gu5PK8gFfN&Expires=1702875328&Signature=d152gh6%2FkamDqItTMX%2FdmYY89aE%3D" -O data_cache.tar.gz
+wget "https://lsh-oss-gpt-spam.oss-cn-shanghai.aliyuncs.com/zhuran/data_cache.tar.gz?OSSAccessKeyId=LTAI5tDuCoTTh6gu5PK8gFfN&Expires=1702885453&Signature=nM3F%2FG3NG%2FCudtfKKbI88rusAEA%3D" -O data_cache.tar.gz
 
